187f3e8e444bcbad58c85fc7a35e6211be1ada74
yago-neves <yagolife1@gmail.com>
2026-03-10 13:22:46 -0300

    Melhorias no app

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,27 @@
-const CACHE = "mudanca-cache-v1";
+const CACHE = "mudanca-v2";
 
 const FILES = [
-    "/",
-    "/index.html",
-    "/manifest.json"
+    "./",
+    "./index.html",
+    "./manifest.json"
 ];
 
 self.addEventListener("install", e => {
+
     e.waitUntil(
         caches.open(CACHE).then(cache => {
             return cache.addAll(FILES);
         })
     );
+
 });
 
 self.addEventListener("fetch", e => {
+
     e.respondWith(
         caches.match(e.request).then(res => {
             return res || fetch(e.request);
         })
     );
+
 });
