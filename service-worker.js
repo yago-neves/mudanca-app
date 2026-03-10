30cfb960571f1d621b6bcade40092157d23878a4
yago-neves <yagolife1@gmail.com>
2026-03-10 12:49:05 -0300

    Adicionando fontes app mudança

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE = "mudanca-cache-v1";
+
+const FILES = [
+    "/",
+    "/index.html",
+    "/manifest.json"
+];
+
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(CACHE).then(cache => {
+            return cache.addAll(FILES);
+        })
+    );
+});
+
+self.addEventListener("fetch", e => {
+    e.respondWith(
+        caches.match(e.request).then(res => {
+            return res || fetch(e.request);
+        })
+    );
+});
